ae2039e1d36eb3b31d96cedda4abe2a4e8fb920c
Timur Mishiev <timurmishiev@gmail.com>
2020-05-12 22:47:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,18 +8,24 @@ class ApplicationController < Sinatra::Base
   end
 
 get '/' do
-    erb :index
+    erb :"/index"
   end
 
-
   helpers do
-    def current_user
-      @user = User.find_by_id(session[:user_id])
-    end
 
     def logged_in?
-      !!session[:user_id]
+      !!current_user
     end
+
+    def current_user
+      @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
+    end
+
+    def logout!
+      session.clear
+    end
+
+
   end
 
 end
